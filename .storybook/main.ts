72293eee15af8e96ff4613c40e3ca7dfd1c1f2f9
Lucifurry <thelucifurry@gmail.com>
2022-10-03 18:38:37 +0700

    Fix: (try 6) broken base path for loading assets of storybook's builded docs

---
+++ .storybook/main.ts
@@ -0,0 +1,38 @@
+const { mergeConfig } = require('vite');
+
+const PATH_BASE = '/warcraft-3-ui/';
+
+
+module.exports = {
+  "stories": [
+    `${__dirname}/../src/**/*.stories.mdx`,
+    `${__dirname}/../src/**/*.stories.@(js|jsx|ts|tsx)`
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    'storybook-dark-mode'
+  ],
+  "framework": "@storybook/vue3",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  },
+  "features": {
+    "storyStoreV7": true
+  },
+  "staticDirs": ['../src/assets'],
+
+  async viteFinal(config, { configType }) {
+    // if (configType === 'DEVELOPMENT') {}
+    if (configType === 'PRODUCTION') {
+      config.base = PATH_BASE;
+    }
+
+    return mergeConfig(config, {
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
+  },
+}
