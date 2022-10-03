7530a6b0f1a16470a343c1a8a019ce2324b99116
TheLucifurry <47812547+TheLucifurry@users.noreply.github.com>
2022-10-03 10:52:05 +0000

    Deploying to docs from  @ 865ffa3d1382e6cf0800678ee36d3bab1aa5cbf7 🚀

---
+++ assets/es.number.is-nan.045d2581.js
@@ -0,0 +1,2 @@
+var N=Object.defineProperty;var a=(r,t)=>N(r,"name",{value:t,configurable:!0});import{Y as e}from"./iframe.23c937dd.js";var i=e;i({target:"Number",stat:!0},{isNaN:a(function(t){return t!=t},"isNaN")});
+//# sourceMappingURL=es.number.is-nan.045d2581.js.map
--- assets/es.number.is-nan.6f93e41c.js
+++
@@ -1,2 +0,0 @@
-var N=Object.defineProperty;var a=(r,t)=>N(r,"name",{value:t,configurable:!0});import{Y as e}from"./iframe.1295a591.js";var i=e;i({target:"Number",stat:!0},{isNaN:a(function(t){return t!=t},"isNaN")});
-//# sourceMappingURL=es.number.is-nan.6f93e41c.js.map
