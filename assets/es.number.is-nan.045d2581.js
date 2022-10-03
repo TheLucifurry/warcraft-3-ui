f2cf4aa916bb7cb27eb4b9b350ce87f99613b7a5
TheLucifurry <47812547+TheLucifurry@users.noreply.github.com>
2022-10-03 11:46:23 +0000

    Deploying to docs from  @ 5f508b39ea1e8d1f27945cad4020147d35a00020 🚀

--- assets/es.number.is-nan.045d2581.js
+++
@@ -1,2 +0,0 @@
-var N=Object.defineProperty;var a=(r,t)=>N(r,"name",{value:t,configurable:!0});import{Y as e}from"./iframe.23c937dd.js";var i=e;i({target:"Number",stat:!0},{isNaN:a(function(t){return t!=t},"isNaN")});
-//# sourceMappingURL=es.number.is-nan.045d2581.js.map
