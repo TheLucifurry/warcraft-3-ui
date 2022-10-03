6b0f5b71dff6b99f6c8d2951e5e9c39a51bf33a7
Lucifurry <thelucifurry@gmail.com>
2022-10-03 13:05:39 +0700

    test

---
+++ docs-build/assets/config.29bbb389.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory.8303276e.js";import"./iframe.d3fca4a9.js";import"./vue.esm-bundler.e58e51d9.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config.29bbb389.js.map
