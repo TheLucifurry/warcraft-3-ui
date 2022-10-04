e726f81e4f1cf842dd2c465ba1401ef1b8f0bc56
Lucifurry <thelucifurry@gmail.com>
2022-10-04 11:57:44 +0700

    Ref: remove `storybook-addon-turbo-build`

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,8 +11,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    'storybook-dark-mode',
-    'storybook-addon-turbo-build'
+    'storybook-dark-mode'
   ],
   "framework": "@storybook/vue3",
   "core": {
