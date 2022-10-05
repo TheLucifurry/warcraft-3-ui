c17b2f0ab878533ecb757582f815a440ec57660e
Lucifurry <thelucifurry@gmail.com>
2022-10-05 10:39:16 +0700

    Fix: `utils:getAssetUrl` - theme must be optional

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,7 @@ export function getAssetPath(path: string, theme: RACE_KEY | 'any' = 'any'): str
   return `${ASSETS_PATH}${theme}/${path}`;
 }
 
-export function getAssetUrl(path: string, theme: RACE_KEY | 'any'): string {
+export function getAssetUrl(path: string, theme?: RACE_KEY | 'any'): string {
   return `url(${getAssetPath(path, theme)})`;
 }
 
